c90791228ba707dc11fe626f8a4889e4b7acd28d
Monique McIntyre <skaiwilliams85@gmail.com>
2022-07-22 17:00:21 -0500

    Installed rtk and configured store

--- src/components/pages/Shop.jsx
+++ src/components/pages/Shop.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import ProductDetails from "../products/ProductDetails";
+import ProductList from "../products/ProductList";
 
 const Shop = () => {
   return (
     <>
       <ProductDetails />
+      <ProductList />
     </>
   );
 };
---
+++ src/components/products/ProductList.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Container from "../helpers/wrapper/Container";
+
+const ProductList = () => {
+  return (
+    <div>
+      <Container>ProductList</Container>
+    </div>
+  );
+};
+
+export default ProductList;
