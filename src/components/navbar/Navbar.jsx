c3dc4e8ba4a38dd5c020f9be6008dd654f686d81
Monique McIntyre <skaiwilliams85@gmail.com>
2022-07-22 00:00:49 -0500

    Added icons to nav

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 import styled from "./Navbar.module.css";
 import Container from "../helpers/wrapper/Container";
-import { GiHamburgerMenu } from "react-icons/gi";
+import { AiOutlineShopping } from "react-icons/ai";
+import { CgHeart } from "react-icons/cg";
+import { NavLink } from "react-router-dom";
 
 const Navbar = () => {
   return (
@@ -9,7 +11,20 @@ const Navbar = () => {
       <Container>
         <nav className={styled.nav__group}>
           <p className={styled.logo}>finest&co</p>
-          <GiHamburgerMenu size="22" color="var(--primary)" />
+
+          <ul className={styled.nav__links}>
+            <li>
+              <NavLink to="/">Home</NavLink>
+            </li>
+            <li>
+              <NavLink to="/shop">Shop</NavLink>
+            </li>
+          </ul>
+
+          <div className={styled.nav__icons}>
+            <CgHeart size="22" color="var(--primary)" />
+            <AiOutlineShopping size="24" color="var(--primary)" />
+          </div>
         </nav>
       </Container>
     </div>
