edc5a73b9cbf503aaf58cbcaa346065a2244470f
Monique McIntyre <skaiwilliams85@gmail.com>
2022-07-21 23:47:50 -0500

    Added hero component

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -1,7 +1,22 @@
 import React from "react";
+import Button from "../helpers/ui/button/Button";
+import styled from "./Hero.module.css";
 
 const Hero = () => {
-  return <div>Hero</div>;
+  return (
+    <section className={styled.background}>
+      <article className={styled.hero}>
+        <h1>
+          <span>extra-ordinary</span> service made <span>affordable</span>
+        </h1>
+        <p className="text">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sed eligendi
+          non qui impedit.
+        </p>
+        <Button>Shop Now</Button>
+      </article>
+    </section>
+  );
 };
 
 export default Hero;
--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import Hero from "../hero/Hero";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <>
+      <Hero />
+    </>
+  );
 };
 
 export default Home;
