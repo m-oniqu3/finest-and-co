7a0594a5a3c2a716640ac2daf10bf8eb865f76c9
Monique McIntyre <skaiwilliams85@gmail.com>
2022-07-27 13:42:48 -0500

    Created portal to render mobile sort menu

--- src/components/filters/Filters.jsx
+++ src/components/filters/Filters.jsx
@@ -1,25 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "./Filters.module.css";
 import Button from "../helpers/ui/button/Button";
 import { BiSortAlt2 } from "react-icons/bi";
 import { MdFilterListAlt } from "react-icons/md";
+import MobileSort from "./MobileSort";
 
 const Filters = () => {
+  const [openSortMenu, setOpenSortMenu] = useState(false);
+
+  const handleSortMenu = () => setOpenSortMenu((state) => !state);
+  console.log(openSortMenu);
+
   return (
-    <div className={styled.filters}>
-      <Button className="secondary">
-        Sort
-        <span>
-          <BiSortAlt2 size="20" />
-        </span>
-      </Button>
-      <Button className="secondary">
-        Filter
-        <span>
-          <MdFilterListAlt size="20" />
-        </span>
-      </Button>
-    </div>
+    <>
+      <div className={styled.filters}>
+        <Button className="secondary" onClick={handleSortMenu}>
+          Sort
+          <span>
+            <BiSortAlt2 size="20" />
+          </span>
+        </Button>
+        <Button className="secondary">
+          Filter
+          <span>
+            <MdFilterListAlt size="20" />
+          </span>
+        </Button>
+      </div>
+
+      {openSortMenu && (
+        <MobileSort
+          openSortMenu={openSortMenu}
+          setOpenSortMenu={setOpenSortMenu}
+        />
+      )}
+    </>
   );
 };
 
---
+++ src/components/filters/MobileSort.jsx
@@ -0,0 +1,19 @@
+import React, { useEffect } from "react";
+import styled from "./MobileSort.module.css";
+import ReactDOM from "react-dom";
+
+const MobileSort = (props) => {
+  useEffect(() => {
+    if (props.openSortMenu) {
+      document.body.style.overflow = "hidden";
+    } else if (!props.openSortMenu) {
+      document.body.style.overflow = "auto";
+    }
+  }, [props]);
+  return ReactDOM.createPortal(
+    <section className={styled.sort}>MobileSort</section>,
+    document.querySelector("#filters")
+  );
+};
+
+export default MobileSort;
