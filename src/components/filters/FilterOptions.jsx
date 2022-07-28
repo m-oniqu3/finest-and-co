0780252d8ac0dca8337703531dd6f04076fe54aa
Monique McIntyre <skaiwilliams85@gmail.com>
2022-07-28 12:39:48 -0500

    Added options to filter by

--- src/components/filters/FilterOptions.jsx
+++ src/components/filters/FilterOptions.jsx
@@ -1,7 +1,53 @@
 import React from "react";
+import styled from "./FilterOptions.module.css";
+import { useSelector } from "react-redux";
+import Button from "../helpers/ui/button/Button";
 
 const FilterOptions = () => {
-  return <div>FilterOptions</div>;
+  const { products } = useSelector((state) => state.products);
+
+  const productCategories = new Set(
+    products.map((product) => product.category)
+  );
+
+  const productCompanies = new Set(products.map((product) => product.company));
+
+  const categoryOptions = Array.from(productCategories).map((category) => {
+    return (
+      <div key={category} className={styled.filter__option}>
+        <input type="checkbox" id={category} />
+        <label htmlFor={category}>{category}</label>
+      </div>
+    );
+  });
+
+  const companyOptions = Array.from(productCompanies).map((company) => {
+    return (
+      <div key={company} className={styled.filter__option}>
+        <input type="checkbox" id={company} />
+        <label htmlFor={company}>{company}</label>
+      </div>
+    );
+  });
+
+  return (
+    <form className={styled.filter}>
+      <div className={styled.filter__group}>
+        <h4>Category</h4>
+        {categoryOptions}
+      </div>
+
+      <div className={styled.filter__group}>
+        <h4>Company</h4>
+        {companyOptions}
+      </div>
+
+      <div className={styled["filter__button-group"]}>
+        <Button className="secondary">Clear</Button>
+        <Button className="primary">Filter</Button>
+      </div>
+    </form>
+  );
 };
 
 export default FilterOptions;
