6549251556eb7c103340d25d71da6c7699c458f4
Monique McIntyre <skaiwilliams85@gmail.com>
2022-08-06 22:10:52 -0500

    Cleaned up cart components

--- src/components/helpers/ui/empty/Empty.jsx
+++ src/components/helpers/ui/empty/Empty.jsx
@@ -2,25 +2,28 @@ import React from "react";
 import styled from "./Empty.module.css";
 import Button from "../button/Button";
 import { useNavigate } from "react-router-dom";
+import Container from "../../wrapper/Container";
 
 const Empty = (props) => {
+  const { heading, text, button } = props;
   const navigate = useNavigate();
 
   //navigate to the specified page/route
   const handleClick = () => navigate(`${props.route}`);
 
   return (
-    <section className={styled.empty}>
-      <figure>
-        <img src={props.image} alt={props.alt} />
-      </figure>
+    <Container>
+      <article className={styled.empty}>
+        <h2>{heading}</h2>
+        <p>{text}</p>
 
-      <div>
-        <h2>{props.heading}</h2>
-        <p>{props.text}</p>
-        {props.button && <Button onClick={handleClick}>{props.button}</Button>}
-      </div>
-    </section>
+        {button && (
+          <Button className="primary" onClick={handleClick}>
+            {button}
+          </Button>
+        )}
+      </article>
+    </Container>
   );
 };
 
--- src/components/pages/Cart.jsx
+++ src/components/pages/Cart.jsx
@@ -1,10 +1,8 @@
 import React, { useEffect } from "react";
 import styled from "../cart/Cart.module.css";
 import { useDispatch, useSelector } from "react-redux";
-import { useNavigate } from "react-router-dom";
 import { calculateTotal } from "../../store/features/cart/cartSlice";
 import CartItem from "../cart/CartItem";
-import { IoIosArrowDropleftCircle } from "react-icons/io";
 import Container from "../helpers/wrapper/Container";
 import CartSummary from "../cart/CartSummary";
 import Empty from "../helpers/ui/empty/Empty";
@@ -12,19 +10,16 @@ import Navbar from "../navbar/Navbar";
 import SignInPrompt from "../auth/SignInPrompt";
 
 const Cart = () => {
-  const navigate = useNavigate();
   const dispatch = useDispatch();
+
   //get cartItems
-  const { cartItems } = useSelector((state) => state.cart);
+  const { cartItems, amountOfItemsInCart } = useSelector((state) => state.cart);
   const { user } = useSelector((state) => state.auth);
 
   //calculate the total everytime the cartItems change
   useEffect(() => {
     dispatch(calculateTotal());
   }, [dispatch, cartItems]);
-
-  //navigate to the previous page
-  const handlePrevious = () => navigate(-1);
 
   // map over each item in cartItems from the store and return a CartItem with the item info
   const cart = cartItems.map((item) => {
@@ -35,39 +30,41 @@ const Cart = () => {
   const emptyText =
     "Your cart is empty, but we can fix that. Visit the shop and start adding items to your cart.";
 
+  //if the user is not logged in, show the sign in prompt
   if (!user?.id) return <SignInPrompt page="cart" />;
 
   return (
     <div>
       <Navbar />
-      <section className={styled.cart__container}>
-        <Container>
-          <div className={styled.cart__heading}>
-            <span onClick={handlePrevious}>
-              <IoIosArrowDropleftCircle
-                size="25"
-                color="var(--secondary-neutral)"
-              />
-            </span>
-            <h4>Your Shopping Cart</h4>
-          </div>
 
-          {/* if cart is empty then show message else show the cartitems */}
-          {emptyCart ? (
-            <Empty
-              heading="Your cart is empty."
-              text={emptyText}
-              button="Start shopping"
-              route="/shop"
-            />
-          ) : (
-            cart
-          )}
-
-          {/* only show cart summary if the cart is not empty */}
-          {!emptyCart && <CartSummary />}
-        </Container>
-      </section>
+      {/* if empty show empty message */}
+      {emptyCart ? (
+        <div className={styled.cart__empty}>
+          <Empty
+            heading="Your cart is empty."
+            text={emptyText}
+            button="Start shopping"
+            route="/shop"
+          />
+        </div>
+      ) : (
+        <section className={styled.cart__container}>
+          <Container>
+            <div className={styled.cart}>
+              <div>
+                <h3 className={styled.cart__heading}>
+                  Order Details <span>{amountOfItemsInCart} items</span>
+                </h3>
+                {cart}
+              </div>
+              {/* only show cart summary if the cart is not empty */}
+              <div className={styled.cart__summary}>
+                {!emptyCart && <CartSummary />}
+              </div>
+            </div>
+          </Container>
+        </section>
+      )}
     </div>
   );
 };
