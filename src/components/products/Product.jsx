45989bcbffed4fc3097762480a99890a0549274a
Monique McIntyre <skaiwilliams85@gmail.com>
2022-08-07 16:05:51 -0500

    Added wishlistSlice and dispatched action to add to wishlist

--- src/components/products/Product.jsx
+++ src/components/products/Product.jsx
@@ -3,10 +3,15 @@ import Color from "../helpers/ui/colour/Color";
 import styled from "./Product.module.css";
 import { useNavigate } from "react-router-dom";
 import { CgHeart } from "react-icons/cg";
+import Button from "../helpers/ui/button/Button";
+import { useDispatch, useSelector } from "react-redux";
+import { addToWishList } from "../../store/features/wishlist/wishlistSlice";
 
 const Product = (props) => {
-  const { product } = props;
+  const { product, wishlist } = props;
   const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const { user } = useSelector((state) => state.auth);
 
   //format for price
   const nf = new Intl.NumberFormat("en-US");
@@ -19,8 +24,19 @@ const Product = (props) => {
   //navigate to product info page for the current product
   const handleClick = () => navigate(`/shop/${product.id}`);
 
+  const wishListHandler = (e) => {
+    e.stopPropagation();
+    if (!user.id) navigate("/account");
+    else dispatch(addToWishList(product));
+  };
+
+  const heightClass = wishlist ? "height" : "normal";
+
   return (
-    <article className={styled.product} onClick={handleClick}>
+    <article
+      className={`${styled.product} ${styled[heightClass]}`}
+      onClick={handleClick}
+    >
       <figure className={styled.product__image}>
         <img src={product.image} alt={product.name} />
       </figure>
@@ -33,10 +49,15 @@ const Product = (props) => {
         <div className={styled.product__colours}>{colours} </div>
 
         <div className={styled.product__group}>
-          <p className={styled.product__price}>$ {nf.format(product.price)}</p>
-          <div className={styled.product__icons}>
-            <CgHeart size="22" />
+          <div className={styled.product__group__info}>
+            <p className={styled.product__price}>
+              $ {nf.format(product.price)}
+            </p>
+            <div className={styled.product__icons} onClick={wishListHandler}>
+              <CgHeart size="22" />
+            </div>
           </div>
+          {wishlist && <Button className="primary">Move to cart</Button>}
         </div>
       </div>
     </article>
---
+++ src/store/features/wishlist/wishlistSlice.js
@@ -0,0 +1,66 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  wishListItems: [],
+  amountOfItemsInWishList: 0,
+  itemIsInList: false,
+  feedback: {
+    image: "",
+    message: "",
+  },
+};
+
+const wishlistSlice = createSlice({
+  name: "wishlist",
+  initialState,
+  reducers: {
+    addToWishList: (state, action) => {
+      //stores the payload in a constant
+      const itemToAdd = action.payload;
+
+      //checks if the payload is already in the  wishListItems array
+      const itemToAddExists = state.wishListItems.find(
+        (item) => item.id === itemToAdd.id
+      );
+
+      //if payload does not exist then create a new object with its values
+      if (!itemToAddExists) {
+        state.wishListItems.push(itemToAdd);
+
+        //Update the feedback and increase the amount of items in the wishlist
+        state.feedback.message = "Item added";
+        state.feedback.image = itemToAdd.imgSrc;
+        state.amountOfItemsInWishList++;
+        // state.itemIsInList = state.wishListItems.includes(itemToAdd);
+      }
+      if (itemToAddExists) {
+        //Update the feedback and increase the amount of items in the wishlist
+        state.message = "Item removed";
+        state.amountOfItemsInWishList--;
+        // state.itemIsInList = state.wishListItems.includes(itemToAddExists);
+
+        //remove the selected item from the array
+        state.wishListItems = state.wishListItems.filter(
+          (item) => item.id !== itemToAddExists.id
+        );
+      }
+    },
+
+    /**checks if the current item is already in the wishlist
+     * returns boolean
+     */
+    checkIfItemIsInWishList: (state, action) => {
+      const item = state.wishListItems.find(
+        (item) => item.id === action.payload
+      );
+      state.itemIsInList = state.wishListItems.includes(item);
+    },
+  },
+});
+
+export const {
+  addToWishList,
+  removeItemFromWishList,
+  checkIfItemIsInWishList,
+} = wishlistSlice.actions;
+export default wishlistSlice.reducer;
