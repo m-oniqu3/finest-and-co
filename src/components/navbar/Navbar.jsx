f38a9ddb4d4a246cfefb072678f23250de79a49f
Monique McIntyre <skaiwilliams85@gmail.com>
2022-08-07 14:33:57 -0500

    Fixed styles for a few components

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -37,7 +37,7 @@ const Navbar = (props) => {
 
   const handleAccount = () => {
     if (!user.id) navigate("/account");
-    setOpenModal((state) => !state);
+    else setOpenModal((state) => !state);
   };
 
   return (
